1954eafbe18d315f2e733d66a1d2775ecc37d316
ashwini0711 <ashwinipawar75760@gmail.com>
2025-06-07 19:52:24 +0530

    Update settings.py

--- resume_matcher/settings.py
+++ resume_matcher/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE':'django.db.backends.mysql',
         'NAME':'resume_matcher',
         'USER':'root',
-        'PASSWORD':'security',
+        'PASSWORD':'**********',
         'HOST':'localhost',
         'PORT':'3306',
         'OPTIONS': {
